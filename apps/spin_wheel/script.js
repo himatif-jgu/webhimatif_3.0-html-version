d70e9e830be97ebab6163f31dfe6373f09f3cdb4
Zidannn10 <zidanazzahra916@gmail.com>
2025-07-29 18:42:58 +0700

    update spin wheels

--- apps/spin_wheel/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("spinButton").addEventListener("click", function() {
-    const wheel = document.getElementById("spinWheel");
-    const randomDegree = Math.floor(Math.random() * 360 + 3600); // Spin 10x + random degrees
-    wheel.style.transform = `rotate(${randomDegree}deg)`;
-});
